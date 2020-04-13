b13963f6b89f74723b2c11d2ccfb1d8fba19fc3c
ohduran <alvaro.duranb@hotmail.com>
2020-04-13 17:36:55 +0200

    SEO basic configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     description:
       "La digitalización de tu empresa, ahora",
     siteUrl: "https://www.alvaroduran.com",
-    image: "/images/logo.png", // Path to your image you placed in the 'static' folder
+    image: "/images/icon.png", // Path to your image you placed in the 'static' folder
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -22,6 +22,24 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-131540516-1",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Álvaro Durán",
+        short_name: "Álvaro Durán",
+        start_url: "/",
+        background_color: "#3b4252",
+        theme_color: "#5e81ac",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
   ],
--- src/layouts/Default.js
+++ src/layouts/Default.js
@@ -1,7 +1,9 @@
 import React from "react"
 import '../global.css'
+import SEO from '../components/SEO'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+
 
 
 export default class extends React.Component {
@@ -9,6 +11,7 @@ export default class extends React.Component {
     render () {
       return (
         <>
+          <SEO />
           <Header />
           <main>
             {this.props.children}
