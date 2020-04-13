8a4021e9f4ca46d998e26d04d56e7d87d748b415
ohduran <alvaro.duranb@hotmail.com>
2020-04-13 17:50:54 +0200

    sitemap and robot.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     title: "Álvaro Durán",
     titleTemplate: "%s · Consultor Digital",
     description:
-      "La digitalización de tu empresa, ahora",
+      "La digitalización de tu empresa, ahora. Mi nombre es Álvaro Durán y ayudo a empresas como la tuya a digitalizarse. Una persona no debería hacer el trabajo de una máquina.",
     siteUrl: "https://www.alvaroduran.com",
     image: "/images/icon.png", // Path to your image you placed in the 'static' folder
   },
@@ -35,12 +35,15 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        icon: "static/images/icon.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-robots-txt',
   ],
 }
