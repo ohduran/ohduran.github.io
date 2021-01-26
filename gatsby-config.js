7b0f58b3e3d6c8208c488917aae765625fc5cbbc
ohduran <alvaro.duranb@hotmail.com>
2021-01-26 22:06:11 +0100

    comment out feed mdx until everything else is done

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,11 @@ dotenv.config();
 const { githubApiQuery } = require("./github-api");
 
 module.exports = {
+  siteMetadata: {
+    title: `Alvaro Duran`,
+    description: `The primary source for my writing.`,
+    siteUrl: `https://alvaroduran.com`,
+  },
   plugins: [
     "gatsby-plugin-postcss",
     {
@@ -35,70 +40,6 @@ module.exports = {
         variables: {
           github_login: process.env.GITHUB_LOGIN,
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-feed-mdx`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url:
-                    site.siteMetadata.siteUrl +
-                    "/essays" +
-                    edge.node.fields.slug,
-                  guid:
-                    site.siteMetadata.siteUrl +
-                    "/essays" +
-                    edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                });
-              });
-            },
-            query: `
-              {
-                allMdx(
-                  sort: { fields: [frontmatter___date], order: DESC }
-                  filter: { frontmatter: { published: { eq: true } } }
-                ) {
-                  nodes {
-                    id
-                    excerpt(pruneLength: 250)
-                    slug
-                    frontmatter {
-                      title
-                      date
-                      summary
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Alvaro Duran's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/essays/",
-          },
-        ],
       },
     },
   ],
