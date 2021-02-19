bfe48284c3433d5039b1937f6bf425bcf78a8d7a
Alvaro <26382770+ohduran@users.noreply.github.com>
2021-02-19 17:53:01 +0100

    Against agile (#97)
    * placeholder
    
    * include rss and mail
    
    * first draft
    
    * finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
+import { RSS, Mail } from "../icons";
 
 const NavBar = ({ className }) => {
   return (
     <nav
       className={`
-        mt-2 md:mt-5 mx-auto grid grid-cols-2 gap-4 py-2 md:py-4
+        mt-2 md:mt-5 mx-auto grid grid-cols-3 gap-4 py-2 md:py-4
         divide-x-2 divide-double divide-nord-8
         text-center text-sm md:text-xl uppercase
         ${className}
@@ -17,6 +18,14 @@ const NavBar = ({ className }) => {
       <Link className="hover:text-nord-10" to="/essays">
         Essays
       </Link>
+      <div className="flex justify-around md:justify-end align-middle">
+        <a href="mailto:alvaro.duran.barata@gmail.com" className="md:mr-5">
+          <Mail className="h-5 md:h-8 text-nord-3" />
+        </a>
+        <Link to="/rss.xml">
+          <RSS className="h-5 md:h-8 text-nord-3" />
+        </Link>
+      </div>
     </nav>
   );
 };
