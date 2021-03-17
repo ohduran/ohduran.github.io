14af9d9c3266fd9773ece8dee49008a12f492a8c
ohduran <alvaro.duranb@hotmail.com>
2021-03-17 12:52:45 +0100

    shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
       lg: "1024px",
       xl: "1280px",
     },
+    boxShadow: {
+      sm: "0 1px 2px 0 #eceff4",
+      DEFAULT: "0 1px 3px 0 #e5e9f0, 0 1px 2px 0 #e5e9f0",
+      md: "0 4px 6px -1px #e5e9f0, 0 2px 4px -1px #e5e9f0",
+      lg: "0 10px 15px -3px #e5e9f0, 0 4px 6px -2px #eceff4",
+      xl: "0 20px 25px -5px #e5e9f0, 0 10px 10px -5px #eceff4",
+      "2xl": "0 25px 50px -12px #d8dee9 ",
+      "3xl": "0 35px 60px -15px #d8dee9",
+      inner: "inset 0 2px 4px 0 #e5e9f0",
+      none: "none",
+    },
     extend: {
       colors: {
         nord: {
