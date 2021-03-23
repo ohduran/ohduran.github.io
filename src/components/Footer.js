04267089a3a0386e1d68a63e510c46b74e7f7545
ohduran <alvaro.duranb@hotmail.com>
2021-03-23 22:29:38 +0100

    font-quaternary and refactor of fonts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,25 +33,25 @@ const Footer = ({ className }) => {
       <main
         className={`w-9/12 mx-auto pt-20 mt-5 border-t border-nord-8 grid grid-cols-9 grid-rows-3 justify-items-center items-center ${className}`}
       >
-        <span className="text-9xl font-family-secondary col-start-2 col-span-2 text-nord-7">
+        <span className="text-9xl font-family-tertiary col-start-2 col-span-2 text-nord-7">
           H
         </span>
-        <span className="text-9xl font-family-secondary col-start-4 text-nord-8">
+        <span className="text-9xl font-family-tertiary col-start-4 text-nord-8">
           E
         </span>
-        <span className="text-9xl font-family-tertiary transform rotate-6 col-start-6 text-nord-13">
+        <span className="text-9xl font-family-quaternary transform rotate-6 col-start-6 text-nord-13">
           L
         </span>
-        <span className="text-9xl text-right font-family-secondary col-start-8 row-start-2 text-nord-9">
+        <span className="text-9xl text-right font-family-tertiary col-start-8 row-start-2 text-nord-9">
           L
         </span>
-        <span className="text-9xl font-family-secondary col-start-7 row-start-3 text-nord-10">
+        <span className="text-9xl font-family-tertiary col-start-7 row-start-3 text-nord-10">
           O
         </span>
         <div className="p-2 col-start-1 col-span-4 row-start-2 row-span-2">
           <p className="text-3xl">
             Let's work{" "}
-            <span className="font-family-secondary hover:text-nord-3">
+            <span className="font-family-tertiary hover:text-nord-3">
               <a
                 href="mailto:alvaro.duran.barata@gmail.com"
                 className="md:mr-5"
@@ -63,11 +63,11 @@ const Footer = ({ className }) => {
           <p className="text-2xl text-center hover:text-nord-3"></p>
         </div>
       </main>
-      <side className="flex justify-around mt-10 mb-5 w-11/12 mx-auto font-family-secondary">
+      <side className="flex justify-around mt-10 mb-5 w-11/12 mx-auto">
         {FooterLinks.map((item) => (
           <div>
             <Link
-              className="no-underline"
+              className="no-underline font-family-quaternary"
               style={{
                 boxShadow: "inset 0 -3px 0 #ebcb8b",
               }}
@@ -75,7 +75,9 @@ const Footer = ({ className }) => {
             >
               {item.to}
             </Link>
-            <p className="mt-1 text-sm opacity-75">{item.description}</p>
+            <p className="mt-1 text-sm opacity-75 font-family-tertiary">
+              {item.description}
+            </p>
           </div>
         ))}
       </side>
