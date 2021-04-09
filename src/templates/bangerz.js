fa57f843d241a9f4de8f8a87ef3b6a3ae740ce70
ohduran <alvaro.duranb@hotmail.com>
2021-04-09 16:37:29 +0200

    responsive design

--- src/templates/bangerz.js
+++ src/templates/bangerz.js
@@ -2,7 +2,7 @@ import { graphql } from "gatsby";
 import React from "react";
 import { InternalLink } from "../atoms";
 import { Penguin } from "../icons";
-import { Footer, NavBar, SEO } from "../organisms";
+import { Footer, SEO } from "../organisms";
 import { Article, CommentBox } from "../organisms";
 
 import * as bangerzStyles from "../styles/bangerz.module.css";
@@ -39,8 +39,8 @@ const BangerzSitesEssay = ({ data }) => {
     >
       <div className="container mx-auto">
         <SEO title={title} description={summary} article={true} />
-        <header className="pt-5 flex items-center">
-          <InternalLink className="justify-self-start outline-none" to="/">
+        <header className="px-1 pt-2 md:pt-5 flex items-center justify-center md:justify-start">
+          <InternalLink className="p-1 md:p-2 rounded-full outline-none" to="/">
             <Penguin
               className="mx-auto h-auto w-10 md:w-16"
               primaryColor="#3a44a2"
@@ -48,17 +48,16 @@ const BangerzSitesEssay = ({ data }) => {
               tertiaryColor="#5e81ac"
             />
           </InternalLink>
-          <NavBar className="justify-self-end mt-3 md:mt-5 text-purple-800" />
         </header>
-        <div className="container mx-auto mt-8">
+        <div className="container mx-auto mt-5 md:mt-8">
           <header
-            className="w-9/12 h-9/12 mx-auto grid"
+            className="grid"
             style={{
               gridTemplateRows: "max-content 1fr max-content",
               background: "linear-gradient(#e8bda4, #3a44a2)",
             }}
           >
-            <h1 className="row-start-1 col-start-1 z-10 flex justify-around text-2xl text-yellow-400 font-secondary">
+            <h1 className="p-2 row-start-1 col-start-1 z-10 flex justify-around text-sm md:text-2xl text-purple-900 font-secondary">
               <span>Á</span>
               <span>L</span>
               <span>V</span>
@@ -66,7 +65,7 @@ const BangerzSitesEssay = ({ data }) => {
               <span>R</span>
               <span>O</span>
             </h1>
-            <h1 className="row-start-3 col-start-1 z-10 flex justify-around text-2xl text-blue-500 font-secondary">
+            <h1 className="p-2 row-start-3 col-start-1 z-10 flex justify-between md:justify-around text-sm md:text-2xl text-yellow-500 font-secondary">
               <span>D</span>
               <span>U</span>
               <span>R</span>
@@ -74,11 +73,11 @@ const BangerzSitesEssay = ({ data }) => {
               <span>N</span>
             </h1>
             <img
-              className="h-144 w-full row-start-1 row-span-3 col-start-1 z-0 object-cover object-center opacity-90 rounded-lg shadow-3xl"
+              className="w-screen md:h-144 md:w-full row-start-1 row-span-3 col-start-1 z-0 object-cover object-center opacity-90 rounded-lg shadow-3xl"
               src={goldCoast}
             />
             <h1
-              className={`text-10xl transform -rotate-30 text-pink-600 font-black col-start-1 row-start-2 self-center justify-self-center z-10 ${bangerzStyles.neon}`}
+              className={`text-6xl sm:text-7xl md:text-9xl lg:text-10xl transform -rotate-30 text-pink-600 font-black col-start-1 row-start-2 self-center justify-self-center z-10 ${bangerzStyles.neon}`}
               style={{
                 fontFamily: "Hastron",
               }}
@@ -87,7 +86,7 @@ const BangerzSitesEssay = ({ data }) => {
             </h1>
           </header>
           <Article className="mt-5 md:mt-20" body={body} />
-          <p className="pt-5 text-xs md:text-sm font-light text-nord-13">
+          <p className="pl-2 pt-5 text-xs md:text-sm font-light text-nord-13">
             Photograph by{" "}
             <a id="special-link" href="https://unsplash.com/@daniel_sturgess">
               Daniel Sturgess
