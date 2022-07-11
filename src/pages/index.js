76f65b9283c45958d22f201b8c412fd2c158427e
ohduran <alvaro.duranb@hotmail.com>
2022-07-11 21:46:46 +0200

    Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,53 @@
 import React from "react";
-import { DefaultLayout } from "../layouts";
-import { InternalLink } from "../atoms";
+import { Link } from "gatsby"
 
 const Home = () => {
   return (
-    <DefaultLayout
-      title="Home"
-      className="grid grid-rows-2 container mx-auto"
-      style={{
-        gridTemplateColumns: "1fr 1fr min-content",
-      }}
-    >
-      <figure className="col-start-1 sm:col-start-2 row-start-1 row-span-2 self-center  z-0 flex">
-        <img
-          className="h-auto w-auto opacity-80 lg:w-1/2 float-right"
-          src="./Cascais.jpg"
-          alt="Writer"
-          style={{
-            mixBlendMode: "overlay",
-          }}
-        />
-      </figure>
-      <main className="col-start-3 col-span-1 row-start-1 row-span-2 z-10 self-center justify-self-center w-7/12 lg:w-6/12 mx-auto text-sm sm:text-base md:text-lg">
-        <p className="text-lg sm:text-xl md:text-2xl font-semibold">
-          Hi, I'm{" "}
-          <span className="font-family-tertiary text-xl sm:text-2xl md:text-3xl">
-            Álvaro Durán
-          </span>
-          .
-        </p>
-        <p className="mt-4 md:mt-5">
-          I'm the Technology Lead at{" "}
-          <a className="text-nord-15" href="https://octans.finance">
-            Octans
-          </a>
-          , and I'm especially interested in the{" "}
-          <span className="font-semibold text-nord-13 font-family-secondary">
-            uncontrollability
-          </span>{" "}
-          of the infrastructure of the world.
-        </p>
-        <p className="mt-1 md:mt-5">
-          I{" "}
-          <a href="https://www.youtube.com/watch?v=S3ebYJxXBRU">
-            speak at conferences
-          </a>{" "}
-          and <InternalLink to="/essays">write essays</InternalLink>.
-        </p>
+    <>
+      <main className="w-11/12 mx-auto">
+        <article className="mt-12 ml-3 mb-12 mr-1 md:grid md:gap-1" style={{
+          gridTemplateColumns: "calc(.1rem + .5vw) max-content 5rem min-content 1fr minmax(30ch,50ch) 1fr",
+          gridTemplateRows: "calc(1rem + 1vw) 50px max-content min-content 1fr"
+        }}>
+          <h1 className="z-10 w-11/12 mx-auto text-4xl font-family-secondary font-bold grid gap-4 grid-cols-7 grid-rows-6 justify-center items-center md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-5">
+            <span className="col-start-2 row-start-1">Á</span>
+            <span className="col-start-3 row-start-1">L</span>
+            <span className="col-start-7 row-start-1">V</span>
+            <span className="col-start-3 row-start-2">A</span>
+            <span className="col-start-4 row-start-3">R</span>
+            <span className="col-start-5 row-start-3">O</span>
+            <span className="col-start-1 row-start-4">D</span>
+            <span className="col-start-2 row-start-5">U</span>
+            <span className="col-start-4 row-start-6">R</span>
+            <span className="col-start-5 row-start-6">Á</span>
+            <span className="col-start-6 row-start-6">N</span>
+          </h1>
+          <div className="hidden md:block border-4 z-0 border-solid border-nord-8 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-5" id="red-box" />
+          <section className="mt-8 mb-8 leading-relaxed font-family-primary text-lg md:col-start-6 md:col-end-7 md:row-start-4 md:row-end-6">
+            <p>Hi, I'm Álvaro Durán.</p>
+            <p className="mt-3">I'm a Senior Software Engineer at <span className="font-semibold" style={{ color: "#0f85fb"}}>Vesto</span>. I'm especially interested in the uncontrollability of the infrastructure of the world.</p>
+          </section>
+          <nav role={"navigation"} className="float-left h-max font-semibold font-family-secondary uppercase bg-nord-8 md:col-start-3 md:col-end-7 md:row-start-3 md:row-end-4" style={{
+            writingMode: "vertical-rl"
+          }}>
+            <ul className="p-4">
+              <li><Link to="/about/">The Writer</Link></li>
+              <li><Link to="/essays/">Essays</Link></li>
+            </ul>
+          </nav>
+          <figure className="self-center z-0 float-left md:col-start-3 md:col-end-5 md:row-start-3 md:row-end-6">
+            <img
+              className="h-max md:h-72 w-auto opacity-50 object-none object-right"
+              src="./ProfilePic.jpg"
+              alt="Writer"
+              style={{
+                mixBlendMode: "overlay",
+              }}
+            />
+          </figure>
+        </article>
       </main>
-      <div
-        id="box"
-        className="border-2 sm:border-4 md:border-6 lg:border-8 border-solid border-nord-8 z-0 m-1 md:m-12 col-start-1 col-span-2 row-start-1 row-span-2"
-      />
-      <aside className="col-start-1 col-span-2 row-start-1 row-span-2 z-10 w-11/12 mx-auto text-2xl sm:text-3xl md:text-5xl font-family-secondary font-bold grid gap-2 grid-cols-7 grid-rows-6 justify-center items-center">
-        <span className="col-start-2 row-start-1">Á</span>
-        <span className="col-start-3 row-start-1">L</span>
-        <span className="col-start-5 row-start-1">V</span>
-        <span className="col-start-3 row-start-3">A</span>
-        <span className="col-start-4 row-start-2">R</span>
-        <span className="col-start-5 row-start-3">O</span>
-        <span className="col-start-6 row-start-3">D</span>
-        <span className="col-start-1 row-start-4">U</span>
-        <span className="col-start-4 row-start-5">R</span>
-        <span className="col-start-5 row-start-6">Á</span>
-        <span className="col-start-6 row-start-6">N</span>
-      </aside>
-    </DefaultLayout>
+  </>
   );
 };
 
